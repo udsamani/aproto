6325537f19df80b47fb694b461f0a00bc098fd71
udsamani <uditpsamani@hotmail.com>
2025-01-09 10:54:47 +0000

    feat: basic varint encoding implementation

---
+++ crates/aproto/src/varint.rs
@@ -0,0 +1,107 @@
+use std::num::NonZeroU64;
+
+use bytes::BufMut;
+
+#[allow(unused)]
+#[inline]
+pub fn encode(mut value: u64, buf: &mut impl BufMut) {
+    // Potential max size for varint is 10 bytes
+    for _ in 0..10 {
+        if value < 0x80 {
+            // Lasty byte: just write the value
+            buf.put_u8(value as u8);
+            break;
+        } else {
+            // More bytes needed:
+            // 1. value & 0x7F: get the lower 7 bits
+            // 2. | 0x80: set the continuation bit
+            buf.put_u8((value & 0x7F) as u8 | 0x80);
+            value >>= 7;
+        }
+    }
+}
+
+#[allow(unused)]
+#[inline]
+pub const fn encoded_len(value: u64) -> usize {
+    // Safe to unwrap because value | 1 is always non-zero
+    // Might be a good idea to use unsafe to optimize this
+    let log2value = NonZeroU64::new(value | 1).unwrap().ilog2();
+    ((log2value * 9 + (64 + 9)) / 64) as usize
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn varint() {
+        fn check(value: u64, encoded: &[u8]) {
+            // Small buffer.
+            let mut buf = Vec::with_capacity(1);
+            encode(value, &mut buf);
+            assert_eq!(buf, encoded);
+
+            // Large buffer.
+            let mut buf = Vec::with_capacity(100);
+            encode(value, &mut buf);
+            assert_eq!(buf, encoded);
+
+            assert_eq!(encoded_len(value), encoded.len());
+        }
+
+        check(2u64.pow(0) - 1, &[0x00]);
+        check(2u64.pow(0), &[0x01]);
+
+        check(2u64.pow(7) - 1, &[0x7F]);
+        check(2u64.pow(7), &[0x80, 0x01]);
+        check(300, &[0xAC, 0x02]);
+
+        check(2u64.pow(14) - 1, &[0xFF, 0x7F]);
+        check(2u64.pow(14), &[0x80, 0x80, 0x01]);
+
+        check(2u64.pow(21) - 1, &[0xFF, 0xFF, 0x7F]);
+        check(2u64.pow(21), &[0x80, 0x80, 0x80, 0x01]);
+
+        check(2u64.pow(28) - 1, &[0xFF, 0xFF, 0xFF, 0x7F]);
+        check(2u64.pow(28), &[0x80, 0x80, 0x80, 0x80, 0x01]);
+
+        check(2u64.pow(35) - 1, &[0xFF, 0xFF, 0xFF, 0xFF, 0x7F]);
+        check(2u64.pow(35), &[0x80, 0x80, 0x80, 0x80, 0x80, 0x01]);
+
+        check(2u64.pow(42) - 1, &[0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x7F]);
+        check(2u64.pow(42), &[0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x01]);
+
+        check(
+            2u64.pow(49) - 1,
+            &[0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x7F],
+        );
+        check(
+            2u64.pow(49),
+            &[0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x01],
+        );
+
+        check(
+            2u64.pow(56) - 1,
+            &[0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x7F],
+        );
+        check(
+            2u64.pow(56),
+            &[0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x01],
+        );
+
+        check(
+            2u64.pow(63) - 1,
+            &[0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x7F],
+        );
+        check(
+            2u64.pow(63),
+            &[0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x01],
+        );
+
+        check(
+            u64::MAX,
+            &[0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x01],
+        );
+    }
+}
